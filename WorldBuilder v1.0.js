776ea7f2401620847712e8b7f46d7f373b346331
Genbu Hase <hasegenbu@gmail.com>
2017-11-29 18:07:12 +0900

    著作権名義を変更。

--- WorldBuilder v1.0.js
+++ WorldBuilder v1.0.js
@@ -1,7 +1,7 @@
 /*/
  *#######################################################################
  *WorldBuilder v1.0
- *Copyright (C) 2017 Genbu Project & Genbu Hase All Rights Reversed.
+ *Copyright (C) 2017 Genbu Project All Rights Reversed.
  *#######################################################################
 /*/
 const WorldBuilder = (function () {
